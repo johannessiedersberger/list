f372a95d51839a1eaea003819c42f37615d912c1
Johannes Siedersberger <36839962+johannessiedersberger@users.noreply.github.com>
2018-02-26 12:01:01 +0100

    Add files via upload

---
+++ List/List/MyLinkedList.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Node
+{
+  public Node next;
+  public int data;
+}
+
+public class MyLinkedList
+{
+  private Node _topElement;
+
+  /// <summary>
+  /// Adds a new Value at the end of the list
+  /// </summary>
+  /// <param name="value"></param>
+  public void Add(int value)
+  {
+    if (_topElement == null)
+    {
+      _topElement = new Node();
+      _topElement.data = value;
+    }
+    else
+    {
+      Node elementToAdd = new Node();
+      elementToAdd.data = value;
+
+      Node current = _topElement;
+      while (current.next != null)
+        current = current.next;
+      current.next = elementToAdd;
+    }
+  }
+
+  /// <summary>
+  /// Checks if the given value is included in the list
+  /// </summary>
+  /// <param name="value"></param>
+  /// <returns></returns>
+  public bool Exists(int value)
+  {
+    for (Node current = _topElement; current != null; current = current.next)
+    {
+      if (current.data == value)
+        return true;
+    }
+    return false;
+  }
+
+  /// <summary>
+  /// Returns the lenght of the list
+  /// </summary>
+  /// <returns></returns>
+  public int Length()
+  {
+    int length = 0;
+    for (Node current = _topElement; current != null; current = current.next)
+    {
+      length++;
+    }
+    return length;
+  }
+
+  /// <summary>
+  /// Returns the Value of the Index in the list
+  /// </summary>
+  /// <param name="index"></param>
+  /// <returns></returns>
+  public int Get(int index)
+  {
+    int position = 0;
+    for (Node current = _topElement; current != null; current = current.next)
+    {
+      if (position == index)
+        return current.data;
+      position++;
+    }
+    throw new IndexOutOfRangeException("The index is not in the list");
+  }
+
+  /// <summary>
+  /// Removes the given value from the list
+  /// </summary>
+  /// <param name="value"></param>
+  public void Remove(int value)
+  {
+    for (Node current = _topElement; current != null; current = current.next)
+    {
+      if (_topElement.data == value)
+      {
+        _topElement = current.next;
+      }
+      else if (current.next != null && current.next.data == value)
+      {
+        current.next = current.next.next;
+      }
+    }
+  }
+}
+
---
+++ List/Test/Test.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test
+{
+  class Test
+  {
+    [Test]
+    public void TestAdd()
+    {
+      MyLinkedList list = new MyLinkedList();
+      list.Add(1);
+      list.Add(2);
+      list.Add(3);
+
+      Assert.That(list.Length(), Is.EqualTo(3));
+      Assert.That(list.Get(0), Is.EqualTo(1));
+      Assert.That(list.Get(1), Is.EqualTo(2));
+      Assert.That(list.Get(2), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void TestExists()
+    {
+      MyLinkedList list = new MyLinkedList();
+      list.Add(1);
+      list.Add(2);
+      list.Add(3);
+
+      Assert.That(list.Exists(1), Is.EqualTo(true));
+      Assert.That(list.Exists(2), Is.EqualTo(true));
+      Assert.That(list.Exists(3), Is.EqualTo(true));
+      Assert.That(list.Exists(5), Is.EqualTo(false));
+    }
+
+    [Test]
+    public void TestLength()
+    {
+      MyLinkedList list = new MyLinkedList();
+      list.Add(1);
+      list.Add(2);
+      list.Add(3);
+
+      Assert.That(list.Length(), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void TestRemove()
+    {
+      MyLinkedList list = new MyLinkedList();
+      list.Add(1);
+      list.Add(2);
+      list.Add(3);
+
+
+      list.Remove(2);
+
+      Assert.That(list.Length(), Is.EqualTo(2));
+      Assert.That(list.Get(0), Is.EqualTo(1));
+      Assert.That(list.Get(1), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void TestGet()
+    {
+      MyLinkedList list = new MyLinkedList();
+      list.Add(1);
+      list.Add(2);
+      list.Add(3);
+
+      Assert.That(list.Get(1), Is.EqualTo(2));
+      Assert.Throws<IndexOutOfRangeException>(() => list.Get(10));
+    }
+  }
+}
